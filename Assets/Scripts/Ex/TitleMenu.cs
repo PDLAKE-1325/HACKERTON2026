f46ddecce8979a466ef201b4a685588929d5463f
PDLAKE-1325 <163096820+PDLAKE-1325@users.noreply.github.com>
2026-07-16 23:26:27 +0900

    a

---
+++ Assets/Scripts/Ex/TitleMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour
+{
+    [SerializeField] string nextSceneName;
+    [SerializeField] GameObject credit;
+
+    public void ToggleCredit()
+    {
+        print("a");
+        if (credit != null)
+            credit.SetActive(!credit.activeSelf);
+    }
+    public void StartGame()
+    {
+        print("b");
+        // SceneManager.LoadScene(nextSceneName);
+    }
+}
